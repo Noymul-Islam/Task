ed7d70392b8c6f110dfb179276cac0f95cb0a02f
shorot <shhoroth125@gmail.com>
2017-11-02 01:21:32 +0600

    hello

---
+++ GreetingServer.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class GreetingServer {
+   //public  ServerSocket serverSocket;
+   
+ public static void main(String [] args) {
+      int port =6061;
+      try {
+            ServerSocket  serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(30000);
+            System.out.println("Waiting for client on port " + 
+            serverSocket.getLocalPort() + "...");
+            Socket server = serverSocket.accept();
+            
+            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+            DataInputStream in = new DataInputStream(server.getInputStream());
+            
+            System.out.println(in.readUTF());
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+               + "\nGoodbye!");
+            server.close();
+            
+          
+      
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
+
+}
